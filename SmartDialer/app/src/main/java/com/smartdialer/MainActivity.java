73f4dd81e14ac38664c0af8b16ed21ecdb9afa1a
novicasarenac <nolesarenac@yahoo.com>
2017-01-12 14:12:15 +0100

    Photo intent activity added

--- SmartDialer/app/src/main/java/com/smartdialer/MainActivity.java
+++ SmartDialer/app/src/main/java/com/smartdialer/MainActivity.java
@@ -1,13 +1,25 @@
 package com.smartdialer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button callNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callNumber = (Button)findViewById(R.id.callNumber);
+    }
+
+    public void takePhoto(View view){
+        Intent activityPhotoIntent = new Intent(this, PhotoIntentActivity.class);
+        startActivity(activityPhotoIntent);
     }
 }
