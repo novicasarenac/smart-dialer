9339d3ad14f262b37ec3c1700a3d3cb435e0d17a
novicasarenac <nolesarenac@yahoo.com>
2017-01-12 17:51:48 +0100

    Starting app for taking picture added to PhotoIntentActivity

--- SmartDialer/app/src/main/java/com/smartdialer/PhotoIntentActivity.java
+++ SmartDialer/app/src/main/java/com/smartdialer/PhotoIntentActivity.java
@@ -1,13 +1,60 @@
 package com.smartdialer;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PhotoIntentActivity extends AppCompatActivity {
+
+    private String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_intent);
+
+        dispatchTakePictureIntent();
+    }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, "jpg", storageDir);
+
+        //save file path
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            //file where photo will go
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this, "com.smartdialer.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 }
